0a73135319a0e5e9734d24f495f0e3210df310f2
ctoulouse <camille.toulouse@octo.com>
2025-02-20 11:46:45 +0100

    installation recipes + routing + strategies + utilisation offlineFallback

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+importScripts(
+		'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+const {setDefaultHandler} = workbox.routing
+const {NetworkFirst} = workbox.strategies
+const {offlineFallback} = workbox.recipes;
+
 self.addEventListener('install', (event) => {
 	console.info('SW: install');
 	event.waitUntil(self.skipWaiting());
@@ -7,3 +14,7 @@ self.addEventListener('activate', (event) => {
 	console.info('SW: activate');
 	event.waitUntil(self.clients.claim());
 });
+
+setDefaultHandler(new NetworkFirst());
+
+offlineFallback();
